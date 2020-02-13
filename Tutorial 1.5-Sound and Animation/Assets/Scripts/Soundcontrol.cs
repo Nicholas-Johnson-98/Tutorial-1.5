f0f24ea19e92e91849511c0ded7772728004b07a
Joey-wassa <57016198+Joey-wassa@users.noreply.github.com>
2020-02-12 20:32:08 -0500

    Bl

---
+++ Tutorial 1.5-Sound and Animation/Assets/Scripts/Soundcontrol.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundcontrol : MonoBehaviour
+{
+
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Tutorial 1.5-Sound and Animation/Assets/Scripts/cat_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat_Controller : MonoBehaviour
+{
+    Animator anim;
+
+
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+    }
+}
